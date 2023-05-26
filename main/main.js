a9a52aef161d7d3409fee7dd6f5e9a93efc08ea8
Bruno Galvao Martins <bruno-g-martins@outlook.com>
2023-05-26 17:30:44 -0300

    correção no media query de entre 325px e 400px, correção do event.remove dos links filhos do dropdown menu

--- main/main.js
+++ main/main.js
@@ -1,1 +1,96 @@
-(()=>{"use strict";new class{constructor(e,t,n){this.menuButton=document.querySelector(e),this.menuList=document.querySelectorAll(t),this.html=document.querySelector("html"),this.events=void 0===n?["touchstart","click"]:n,this.openMenu=this.openMenu.bind(this),this.handleOutsideClick=this.handleOutsideClick.bind(this)}openMenu(e){"touchstart"===e.type&&e.preventDefault(),this.menuList.forEach((e=>{e.classList.toggle("ativo")})),this.menuButton.classList.toggle("ativo");const t=Array.from(document.querySelectorAll(".menu-categorias-container a")),n=document.querySelectorAll(".sub-menu");function i(e){e.preventDefault(),n.forEach((e=>{e.classList.add("ativo")})),this.eventRemoved||(t.forEach((e=>{e.removeEventListener("click",i)})),this.eventRemoved=!0)}t.forEach((e=>{e.addEventListener("click",i)}))}handleOutsideClick(e){const t=document.querySelector(".menu-mobile"),n=document.querySelector(".menu-categorias-container"),i=document.querySelector(".menu-login");var s,o;s=e,o=()=>{setTimeout((()=>{this.menuList.forEach((e=>{e.classList.remove("ativo")}))}))},[t,n,i].some((e=>e.contains(s.target)))||o()}addMenuMobileEvents(){this.events.forEach((e=>{this.menuButton.addEventListener(e,this.openMenu),this.html.addEventListener(e,this.handleOutsideClick)}))}init(){return this.menuButton&&this.menuList&&this.addMenuMobileEvents(),this}}('[data-menu="button"]','[data-menu="list"]').init()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/modulos/menu-mobile.js":
+/*!***********************************!*\
+  !*** ./js/modulos/menu-mobile.js ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ MenuMobile)\n/* harmony export */ });\nclass MenuMobile {\r\n  constructor(menuButton, menuList, events) {\r\n    this.menuButton = document.querySelector(menuButton);\r\n    this.menuList = document.querySelectorAll(menuList);\r\n    this.html = document.querySelector(\"html\");\r\n\r\n    if (events === undefined) {\r\n      this.events = [\"touchstart\", \"click\"];\r\n    } else {\r\n      this.events = events;\r\n    }\r\n    this.openMenu = this.openMenu.bind(this);\r\n    this.handleOutsideClick = this.handleOutsideClick.bind(this);\r\n  }\r\n\r\n  openMenu(event) {\r\n    if (event.type === \"touchstart\") event.preventDefault();\r\n    this.menuList.forEach((element) => {\r\n      element.classList.toggle(\"ativo\");\r\n    });\r\n    this.menuButton.classList.toggle(\"ativo\");\r\n\r\n    const links = Array.from(\r\n      document.querySelectorAll(\".menu-categorias-container a\")\r\n    );\r\n    const submenu = document.querySelectorAll(\".sub-menu\");\r\n    links.forEach((link) => {\r\n      link.addEventListener(\"click\", activeSubMenu);\r\n    });\r\n    function activeSubMenu(event) {\r\n      event.preventDefault();\r\n      submenu.forEach((element) => {\r\n        element.classList.add(\"ativo\");\r\n      });\r\n      if (!this.eventRemoved) {\r\n        links.forEach((link) => {\r\n          link.removeEventListener(\"click\", activeSubMenu);\r\n        });\r\n        this.eventRemoved = true;\r\n      }\r\n    }\r\n  }\r\n  handleOutsideClick(event) {\r\n    const buttonMenu = document.querySelector(\".menu-mobile\");\r\n    const listMenu = document.querySelector(\".menu-categorias-container\");\r\n    const loginMenu = document.querySelector(\".menu-login\");\r\n\r\n    const targetElements = [buttonMenu, listMenu, loginMenu];\r\n\r\n    handleOutsideClick(targetElements, event, () => {\r\n      setTimeout(() => {\r\n        this.menuList.forEach((element) => {\r\n          element.classList.remove(\"ativo\");\r\n        });\r\n      });\r\n    });\r\n  }\r\n  addMenuMobileEvents() {\r\n    this.events.forEach((event) => {\r\n      this.menuButton.addEventListener(event, this.openMenu);\r\n      this.html.addEventListener(event, this.handleOutsideClick);\r\n    });\r\n  }\r\n\r\n  init() {\r\n    if (this.menuButton && this.menuList) {\r\n      this.addMenuMobileEvents();\r\n    }\r\n    return this;\r\n  }\r\n}\r\nfunction handleOutsideClick(elementTargets, events, callback) {\r\n  const insideClick = elementTargets.some((elementTarget) => {\r\n    return elementTarget.contains(events.target);\r\n  });\r\n  if (!insideClick) {\r\n    callback();\r\n  }\r\n}\r\n\n\n//# sourceURL=webpack://meu_foot/./js/modulos/menu-mobile.js?");
+
+/***/ }),
+
+/***/ "./js/script.js":
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modulos_menu_mobile_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modulos/menu-mobile.js */ \"./js/modulos/menu-mobile.js\");\n\r\n\r\nconst menuMobile = new _modulos_menu_mobile_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]('[data-menu=\"button\"]', '[data-menu=\"list\"]');\r\nmenuMobile.init();\r\n\n\n//# sourceURL=webpack://meu_foot/./js/script.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./js/script.js");
+/******/ 	
+/******/ })()
+;
